4abf2886c83a005742c6c061f0d88e35174163c9
Anthony Zaino <44411152+anthonyzaino88@users.noreply.github.com>
2023-09-21 09:02:17 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
 
-const container = document.getElementById("products-list-container");
-fetch("./pages/products.html")
-    .then((response) => response.text())
-    .then((html) => {
-        container.innerHTML = html;
-    })
-    .catch((error) => {
-        console.error("Error fetching component:", error);
-    });
