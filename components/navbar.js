ca207ee0628aec8ccb1c23cedd2e8cfcfa497bce
Anthony Zaino <44411152+anthonyzaino88@users.noreply.github.com>
2023-10-03 09:32:39 -0400

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -44,7 +44,7 @@ class BottomNavbar extends HTMLElement {
           <img src="../assets/images/home.svg" alt="home" class="icon-color">
             <span class="nav-icon"></span><br>Home
           </a>
-          <a class="nav-link" href="/products.html">
+          <a class="nav-link" href="https://anthonyzaino88.github.io/sp-app/pages/products.html">
           <img src="../assets/images/fan-white.svg" alt="Products" class="icon-color">
             <span class="nav-icon"></span><br>Products
           </a>
