241e748c30b98817ef074cb75de2ce757e7949d9
Anthony Zaino <44411152+anthonyzaino88@users.noreply.github.com>
2023-10-18 16:37:49 -0400

    Update main.js
    Search Function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+  // Function to handle search result clicks
+  function handleSearchResultClick(product) {
+    const url = `../pages/products.html?product=${encodeURIComponent(product)}`;
+    // Redirect the user to the product list page with the selected parameters
+    window.location.href = url;
+  }
 
+  // You can add event listeners for search result elements here if needed
