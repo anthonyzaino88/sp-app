e9c9c7f0fe4b300990dcd2cafd8bd41fb0c0159b
Anthony Zaino <44411152+anthonyzaino88@users.noreply.github.com>
2023-10-24 16:23:22 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+// Define the cache name and resources to cache
+const cacheName = 'my-pwa-cache-v5';
+const cacheResources = [
+  '/',
+  '/index.html',
+  '/assets/icons/',
+  '/assets/images/',
+  '/assets/css/',
+  '/components/',
+  '/data/',
+  '/js/',
+  '/pages/',
+  '/globleStyles.css'
+
+  // Add more resources to cache here
+];
+
+// Install event: Cache resources when the service worker is installed
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then((cache) => {
+        return cache.addAll(cacheResources);
+      })
+  );
+});
+
+// Fetch event: Serve cached resources if available, otherwise fetch from the network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+  );
+});
